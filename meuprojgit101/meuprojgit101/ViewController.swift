911ad2480f65c7cc8ea6ae0eb4ca502722272a0f
Bruna Costa <brunafranca704@gmail.com>
2020-03-30 15:46:56 -0300

    minha primeira alteração

--- meuprojgit101/meuprojgit101/ViewController.swift
+++ meuprojgit101/meuprojgit101/ViewController.swift
@@ -13,8 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("O projeto já começou")
     }
 
+    
+    
+    
 
 }
 
